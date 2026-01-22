22fb262fa6fbc2e131357c91bb50700e212f3c1f
Rambhupal <rambhupalreddy369@gmail.com>
2026-01-22 10:15:18 +0530

    updated changes

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: true, // allow all origins dynamically
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
